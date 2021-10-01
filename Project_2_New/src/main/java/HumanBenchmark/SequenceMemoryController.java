930a97d643605a9ccba1507b9db70892443e9c39
Zhibin "Bing" Hong <hongzhibin1987@gmail.com>
2021-10-01 12:36:48 -0600

    10/1 a

--- Project_2_New/src/main/java/HumanBenchmark/SequenceMemoryController.java
+++ Project_2_New/src/main/java/HumanBenchmark/SequenceMemoryController.java
@@ -1,6 +1,9 @@
 package HumanBenchmark;
 
+import javafx.animation.Animation;
 import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +19,7 @@ import javafx.util.Duration;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class SequenceMemoryController extends MiniGame {
 
@@ -29,6 +33,7 @@ public class SequenceMemoryController extends MiniGame {
     private ArrayList<Button> newButtons =new ArrayList<Button>();
     private ArrayList<Button> userButtons =new ArrayList<Button>();
     private int lvl;
+    private boolean noError = true;
 
     public SequenceMemoryController(/*String n, String unit, boolean inverse*/) throws Exception {
         //super(n, unit, inverse);
@@ -53,6 +58,8 @@ public class SequenceMemoryController extends MiniGame {
     public void setlvl(int lvl) {
         this.lvl = lvl;
     }
+
+    public void setNoError(boolean noError){ this.noError = noError;};
 
 
     public void ActionMenuMainPage(ActionEvent actionEvent) throws IOException {
@@ -84,10 +91,32 @@ public class SequenceMemoryController extends MiniGame {
 
     }
 
-
+    // get all buttons in allbuttons
     public void gameStart(){
-
+        setAllButtons(allButtons);
+        allButtons.add(btn0);
+        allButtons.add(btn1);
+        allButtons.add(btn2);
+        allButtons.add(btn3);
+        allButtons.add(btn4);
+        allButtons.add(btn5);
+        allButtons.add(btn6);
+        allButtons.add(btn7);
+        allButtons.add(btn8);
     }
+    // boolean noError = ture;
+    // if noError == true:
+        // for loop, add i=lvl items into newButtons, allow more items, just use regular random. allow duplication
+        // display newButtons in the newButtons, with an order;
+        // ask user to clickbuttons, moves = lvl;
+        // do an equals() match;
+            // if userButton.equalsto(newButtons)
+                // lvl++
+                // moves =0;
+                // clearArray();
+            // else
+                // noError = false, gameOverpopuop();
+
 
     public void actionBtnStart(ActionEvent actionEvent) {
         setAllButtons(allButtons);
@@ -100,7 +129,61 @@ public class SequenceMemoryController extends MiniGame {
         allButtons.add(btn6);
         allButtons.add(btn7);
         allButtons.add(btn8);
+
+        lvl = 1;
+        setlvl(lvl);
+        setNoError(noError);
+        setUserButtons(userButtons);
+        setNewButtons(newButtons);
+
+        setupCanvas(lvl);
     }
+
+
+    public void setupCanvas(int lvl){
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(allButtons.size());
+        newButtons.add(allButtons.get(randomIndex));
+        System.out.println(newButtons + "\n");
+
+        for (int j = 0; j <newButtons.size(); j++) {
+            System.out.print(newButtons.get(j));
+            newButtons.get(j).setStyle("-fx-base: #0096FF");
+            PauseTransition pause = new PauseTransition(Duration.seconds(2));
+            pause.play();
+            int finalJ = j;
+            pause.setOnFinished(e -> {
+                newButtons.get(finalJ).setStyle(null);
+            });
+
+//        }
+
+//        System.out.print("lvl in setupCanvas: "+ lvl + "\n");
+//        lvl =1;
+//        Random rand = new Random();
+//        for (int i = 0; i <lvl; i++){
+//            int randomIndex = rand.nextInt(allButtons.size());
+//            newButtons.add(allButtons.get(randomIndex));
+//        }
+//
+//        System.out.print(newButtons);
+//
+//        for (int j = 0; j <lvl; j++){
+//            System.out.print(newButtons.get(j));
+//            newButtons.get(j).setStyle("-fx-base: #0096FF");
+//            PauseTransition pause = new PauseTransition(Duration.seconds(1));
+//            int finalJ = j;
+//            pause.setOnFinished(e ->{
+//                newButtons.get(finalJ).setStyle(null);
+//            });
+//            pause.play();
+//        }
+
+    }
+
+
+    }
+
 
     public void actionBtnReset(ActionEvent actionEvent) {
     }
